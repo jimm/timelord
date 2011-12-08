f532a0fb4b363aed2e422bcce9dfd8c0e17d8724
Jim Menard <jim@jimmenard.com>
2011-12-08 16:44:54 -0500

    fixed money_str and new tests

--- test/unit/invoice_test.rb
+++ test/unit/invoice_test.rb
@@ -3,8 +3,11 @@ require 'test_helper'
 class InvoiceTest < ActiveSupport::TestCase
 
   test "money to string" do
+    assert_equal "$0.00", Invoice.money_str(0)
+    assert_equal "$0.03", Invoice.money_str(3)
     assert_equal "$0.23", Invoice.money_str(23)
     assert_equal "$1.23", Invoice.money_str(123)
+    assert_equal "$1.03", Invoice.money_str(103)
     assert_equal "$1,234.56", Invoice.money_str(123456)
   end
 end
