a063a6e040fb2759fc8d21c53495205181e67e7b
Jim Menard <jim@jimmenard.com>
2011-12-15 13:03:09 -0500

    keep more versions of database around

--- bin/db_backup.sh
+++ bin/db_backup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 d=production.sqlite3
+dest_dir=/Users/jimm/Documents/Dropbox/backups
 
-cp $(dirname $0)/../db/$d ~jimm/Documents/Dropbox/backups/timelord.$d
+# Keep two older backups
+if [ -f $dest_dir/timelord.1.$d ] ; then
+    mv $dest_dir/timelord.1.$d $dest_dir/timelord.2.$d
+fi
+if [ -f $dest_dir/timelord.$d ] ; then
+    mv $dest_dir/timelord.$d $dest_dir/timelord.1.$d
+fi
+
+cp $(dirname $0)/../db/$d $dest_dir/timelord.$d
