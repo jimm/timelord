925816b4a4c5da5466dda32637a25f0b72725ee2
Jim Menard <jim@jimmenard.com>
2011-12-17 18:14:13 -0500

    read data from existing prod db into seeds.rb

---
+++ bin/adhoc/seed_from_db.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+#
+# Reads production.sqlite3 and outputs a seed file.
+
+require File.dirname(__FILE__) + '/../../config/environment'
+
+class String
+  def q
+    "'" + self.gsub(/'/, "''") + "'"
+    end
+end
+
+Location.all.each do |loc|
+  puts "loc = Location.create(name: #{loc.name.q})"
+  loc.codes.each do |code|
+    puts "  code = Code.create(location: loc, code: #{code.code.q}, name: #{code.name.q})"
+    code.work_entries.each do |w|
+      puts "    WorkEntry.create(code: code, worked_at: '#{w.worked_at.to_s(:db)}', minutes: #{w.minutes || 0}, rate_cents: #{w.rate_cents || 'nil'}, fee_cents: #{w.fee_cents || 'nil'}, note: #{w.note.q || 'nil'})"
+    end
+  end
+end
